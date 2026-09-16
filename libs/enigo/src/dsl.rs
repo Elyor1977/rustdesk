88f8386b96f4085d6464aded702f27891250ad01
Elyor Qilichev <elyor77q@gmail.com>
2026-09-16 17:42:18 +0500

    Fix Linux clippy errors

--- libs/enigo/src/dsl.rs
+++ libs/enigo/src/dsl.rs
@@ -37,7 +37,7 @@ impl Error for ParseError {
 }
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.to_string())
+        f.write_str(self.description())
     }
 }
 
@@ -180,5 +180,10 @@ mod tests {
             tokenize("{+CTRL}{{this}} is going to fail}"),
             Err(ParseError::UnmatchedClose)
         );
+    }
+
+    #[test]
+    fn display_error() {
+        assert_eq!(ParseError::UnknownTag("TEST".into()).to_string(), "Unknown tag");
     }
 }
